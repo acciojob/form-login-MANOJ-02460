6e091d377cd692ac34b10d5568505b9051b50a5d
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-06-30 07:06:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.querySelector('[name="fname"]').value;
+	const lastName = document.querySelector('[name="lname"]').value;
+
+	alert(`First Name: ${firstName}, Last Name: ${lastName}`)
 
 }
